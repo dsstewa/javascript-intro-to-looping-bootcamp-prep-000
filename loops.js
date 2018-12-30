246921b5d33b61347e5dc5ca01b09b3b7344bf7c
David Stewart <davidstewart@gmail.com>
2018-12-30 21:48:16 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -4,7 +4,7 @@ var array2 = []
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     
-    array2.push("I am ${i} strange loops")
+    array2.push(`I am ${i} strange loops`)
     
     }
 }
