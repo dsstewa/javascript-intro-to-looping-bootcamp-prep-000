8895cbe9f1208b486485947eaf4fac7f038345aa
David Stewart <davidstewart@gmail.com>
2018-12-30 20:15:19 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,12 +2,7 @@
 function forLoop() {
   for (let i = 0; i < 25; i++) {
     console.log(i)
-    if(i = 1) {
-      console.log("i = 1")
-    } else {
-      console.log(i)
-      
     }
-    }
-  }
+}
+  
 
