32e23f59cdb902ef94718dd5fb2845a456de1966
David Stewart <davidstewart@gmail.com>
2018-12-30 22:15:28 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,20 @@
 
-
-
+function forLoop(array) {
+  for (let i = 1; i < 26; i++) {
+  
+    if (i < 26) {
+      
+      if(i === 1) {
+      array.push("I am 1 strange loop.")
+    }
+      else if(i < 26) {
+        array.push(`I am ${i} strange loops.`)
+      }
+      else {
+        return
+      }
+    }
+  }
+}
    
-      
-      
-      
-
-
 
