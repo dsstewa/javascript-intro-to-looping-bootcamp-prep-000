46572bf24f0596b095b40ba82b895dd7c4967ce6
David Stewart <davidstewart@gmail.com>
2018-12-30 20:06:32 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,9 +3,9 @@ function forLoop() {
   for (let i = 0; i < 25; i++) {
     console.log(i)
     if(i = 1) {
-      return console.log("i = 1")
+      console.log("i = 1")
     } else {
-      return console.log(i)
+      console.log(i)
       
     }
     }
