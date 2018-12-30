5fb140e06c582ece1dee4d5ab67e189d985d0794
David Stewart <davidstewart@gmail.com>
2018-12-30 21:52:33 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -5,7 +5,8 @@ function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i < 10) {
       if(i = 1)
-      array2.push()
+      array2.push("I am 1 strange loop.") 
+    } else { 
     }
     
     array2.push(`I am ${i} strange loops`)
